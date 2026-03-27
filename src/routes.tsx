c407a335f8ed04df047d8138c343a7b0661f5278
yoojanghun <yoojanghun@naver.com>
2026-03-27 09:36:43 +0900

    feat: initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import { Dashboard } from "./pages/Dashboard";
+import { Statistics } from "./pages/Statistics";
+import { AudioManagement } from "./pages/AudioManagement";
+import { FeedWater } from "./pages/FeedWater";
+import { Ventilation } from "./pages/Ventilation";
+import { Layout } from "./components/Layout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: Dashboard },
+      { path: "statistics", Component: Statistics },
+      { path: "audio", Component: AudioManagement },
+      { path: "feed-water", Component: FeedWater },
+      { path: "ventilation", Component: Ventilation },
+    ],
+  },
+]);
